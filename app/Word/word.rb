868aea18118d3982678aa40135367d7ffb2b0b36
Pablo Kang <pablokang@gmail.com>
2010-05-18 01:22:55 -0700

    Link Mightyverse results to video -- tbd: transcode flv movies to something that an iphone can play Style results for iphone

--- app/Word/word.rb
+++ app/Word/word.rb
@@ -1,6 +1,3 @@
-# The model has already been created by the framework, and extends Rhom::RhomObject
-# You can add more methods here
-
 class Word
   @@wordnik_url_prefix = "http://api.wordnik.com/api/word.json/"
   @@wordnik_headers = {'api_key' => Rho::RhoConfig.wordnik_key}
