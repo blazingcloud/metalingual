b06009df2ca5eee3c0159c4e5e2e0a7d055f71f2
Pablo Kang <pablokang@gmail.com>
2010-05-20 15:54:45 -0700

    Mightyverse API key

--- app/Word/word.rb
+++ app/Word/word.rb
@@ -1,7 +1,7 @@
 class Word
   @@wordnik_url_prefix = "http://api.wordnik.com/api/word.json/"
   @@wordnik_headers = {'api_key' => Rho::RhoConfig.wordnik_key}
-  @@mightyverse_url_prefix = "http://mightyverse-staging.heroku.com/search/target_language/en.json?dimension=400x300&content_type=video/quicktime&term="
+  @@mightyverse_url_prefix = "http://mightyverse-staging.heroku.com/search/target_language/en.json?dimension=400x300&content_type=video/quicktime&api_key=#{Rho::RhoConfig.mightyverse_key}&term="
 
   attr_reader :wordnik_definitions, :mightyverse_definitions
 
